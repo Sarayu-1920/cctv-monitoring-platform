56d104b1450412bd92e70798fa7da5404ef47fe6
Sarayu-1920 <kodimalasarayureddy@gmail.com>
2026-02-15 17:54:59 +0530

    Initial project setup with SpringBoot structure and basic authentication module

---
+++ src/main/java/com/cctv/monitoring/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.cctv.monitoring.controller;
+
+import com.cctv.monitoring.entity.User;
+import com.cctv.monitoring.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User loginUser, HttpSession session) {
+
+        Optional<User> user;
+        user = userRepository.findByUsername(loginUser.getUsername());
+
+        if (user.isPresent() && user.get().getPassword().equals(loginUser.getPassword())) {
+
+            session.setAttribute("user", user.get());
+            return ResponseEntity.ok("Login Successful");
+
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Invalid Credentials");
+        }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpSession session) {
+        session.invalidate();
+        return ResponseEntity.ok("Logged Out");
+    }
+}
---
+++ src/test/java/com/cctv/monitoring/CctvMonitoringPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cctv.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CctvMonitoringPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
